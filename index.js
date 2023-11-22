fdd906cc8eda13c0107648ac1503db238078c7f6
somya <somyaagarwal3745@gmail.com>
2023-11-22 19:08:55 +0530

    added

---
+++ index.js
@@ -0,0 +1,27 @@
+// console.log('hello world');
+
+// primitive data-type
+// nn bb ss u primitive datatypes in javasrcript
+let a=null;//iske ander kuch nahi h and koi cheez null h toh bolna padega null h khali nahi chor skte h
+let b=345;
+let c=true;
+let d = BigInt(546)+BigInt(446);
+let e= "Harry"
+let f = Symbol("I am a nice symbol")
+let g = undefined //isko abhi kuch nahi diya h , this can be written as (let g)  is consider as undefined 
+console.log(a,b,c,d,e,f,g)
+console.log(typeof e)
+
+//Objects -non primitive in javascript
+
+const item ={
+    "harry": true,
+    "somya": false,
+    "arti":67,
+    "rohan":undefined
+}
+console.log(item["harry"])
+console.log(item["arti"])
+
+
+
